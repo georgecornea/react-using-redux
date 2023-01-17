5326b307ad37a376016bac4c4414e854bc0cd77c
George Cornea <george.cornea@outlook.com>
2023-01-16 18:59:25 -0800

    Create store.js file (create store object and export) Modiefied index.js to add Provider to the whole app

---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import { createStore } from 'redux';
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        state: state.counter + 1,
+      };
+    case 'DECREMENT':
+      return {
+        state: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+export default store;
